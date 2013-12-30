1d5d56099d9fee700d93086a5ea6334ddd62b90c
Pecherkin Eugene <e.pecherkin@undev.ru>
2013-12-30 12:43:07 +0400

    shotgun

--- life.js
+++ life.js
@@ -1,8 +1,9 @@
 N = 100
 
-world = []
 dead = "dead"
 life = "life"
+
+world = []
 
 function initWorld() {
   for(var i = 0; i < N; ++i) {
@@ -11,11 +12,48 @@ function initWorld() {
       world[i].push(false)
     }
   }
-  world[1][2] = true
-  world[2][3] = true
-  world[3][1] = true
-  world[3][2] = true
-  world[3][3] = true
+  world[51][52] = true
+  world[52][53] = true
+  world[53][51] = true
+  world[53][52] = true
+  world[53][53] = true
+
+  world[6][2]   = true
+  world[6][3]   = true
+  world[5][2]   = true
+  world[5][3]   = true
+  world[4][12]  = true
+  world[5][12]  = true
+  world[6][12]  = true
+  world[3][13]  = true
+  world[7][13]  = true
+  world[2][14]  = true
+  world[8][14]  = true
+  world[2][15]  = true
+  world[8][15]  = true
+  world[5][16]  = true
+  world[3][17]  = true
+  world[7][17]  = true
+  world[4][18]  = true
+  world[5][18]  = true
+  world[6][18]  = true
+  world[5][19]  = true
+  world[6][22]  = true
+  world[7][22]  = true
+  world[8][22]  = true
+  world[6][23]  = true
+  world[7][23]  = true
+  world[8][23]  = true
+  world[5][24]  = true
+  world[9][24]  = true
+  world[4][26]  = true
+  world[5][26]  = true
+  world[9][26]  = true
+  world[10][26] = true
+  world[7][36]  = true
+  world[8][36]  = true
+  world[7][37]  = true
+  world[8][37]  = true
 }
 
 function initWorldLayout() {
@@ -50,6 +88,7 @@ function renderWorld() {
 
 function lifeCycle(){
   var newWorld = []
+
   for(var i = 0; i < N; ++i ) {
     newWorld.push([])
     for(var j = 0; j < N; ++j) {
