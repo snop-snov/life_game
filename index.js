4e2ae38d872e948b555931789e8ad0e1a3fa37e6
Dmitry Chernyatiev <d.chernyatiev@gmail.com>
2013-12-30 13:27:17 +0400

    state3

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
   console.log("init");
   initContent();
   initWorld();
+  initWorldState();
 });
 
 function initContent(){
@@ -28,11 +29,25 @@ function initWorld() {
   for (var i = 0; i < rows; ++i) {
     world.push([]);
     for (var j = 0; j < cols; ++j){
-      world[i][j] = true;
-      $("#" + i + "-" + j).addClass('death');
+      world[i][j] = false;
+    }
+  }
+  world[50][50] = true;
+  world[51][51] = true;
+  world[52][51] = true;
+  world[52][50] = true;
+  world[52][49] = true;
+}
+
+function initWorldState(){
+  for (var i = 0; i < rows; ++i) {
+    for (var j = 0; j < cols; ++j){
+      if (world[i][j])
+        $("#" + i + "-" + j).removeClass(death).addClass(life);
+      else
+        $("#" + i + "-" + j).removeClass(life).addClass(death);
     }
   }
 }
-
 
 
